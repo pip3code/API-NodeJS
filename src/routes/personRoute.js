9da42d2465103419f7d8136e0388ce201e99164f
Diego Oliveira <diegusoliv@icloud.com>
2022-05-16 13:48:22 -0300

    ajustes

---
+++ src/routes/personRoute.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/personController')
+
+router.post('/', controller.post);
+
+router.put('/:id', controller.put);
+
+router.delete('/:id', controller.delete);
+
+router.get('/:id', controller.get)
+
+module.exports = router;
